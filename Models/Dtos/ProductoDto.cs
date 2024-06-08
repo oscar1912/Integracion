41aad25f32aa23e862a132d09dad6b06eb68da9b
Oscar Sosa <anmafc40@hotmail.com>
2024-06-08 18:26:09 -0400

    Agregar Dto a clase Producto e implementar en el controlador

---
+++ Models/Dtos/ProductoDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models.Dtos
+{
+    public class ProductoDto
+    {
+       public String NombreProducto { get; set; }
+        public String Categoria { get; set; }
+        public String Marca { get; set; }
+        public Double Precio { get; set; } 
+    }
+}
