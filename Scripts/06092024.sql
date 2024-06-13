20a6744fc04c68b9cc69d399f851bd8f6437decf
Oscar Sosa <anmafc40@hotmail.com>
2024-06-12 23:21:00 -0400

    Agregar Script de inserts de la Tabla Producto

---
+++ Scripts/06092024.sql
@@ -0,0 +1,5 @@
+INSERT into Producto(NombreProducto,CategoriaId,MarcaId,Precio,Costo)
+VALUES ('HP - Envy',1,1,2000,1500);
+
+INSERT into Producto(NombreProducto,CategoriaId,MarcaId,Precio,Costo)
+VALUES ('HP - EliteBook',1,1,2500,1800);
